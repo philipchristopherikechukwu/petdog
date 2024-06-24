ee292192cc8fe2a3eba73b898ff410f41e0ca5c1
philipchristopherikechukwu <108722688+philipchristopherikechukwu@users.noreply.github.com>
2024-06-24 09:29:00 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>complete responsive food website design tutorial </title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header>
+
+    <a href="#" class="logo"><i class="fas fa-utensils"></i>PETluvs_</a>
+
+    <div id="menu-bar" class="fas fa-bars"></div>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#speciality">speciality</a>
+        <a href="#popular">popular</a>
+        <a href="#gallery">gallery</a>
+        <a href="#review">review</a>
+        <a href="#order">order</a>
+    </nav>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h3>PETS made with love</h3>
+        <p>welcome to the world of man's best friend. your best friend awaits you.</p>
+        <a href="#" class="btn">order now</a>
+    </div>
+
+    <div class="image">
+        <img src="images/akita-700x700.jpg" alt="" style="border-radius:100px;">
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- speciality section starts  -->
+<section class="gallery" id="gallery">
+
+    <h1 class="heading"> our special <span> BREEDS </span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/download%20(5).jpg" alt="">
+            <div class="content">
+                <h3>Available</h3>
+                <p>ATIKA WOLF DOG</p>
+                <a href="#" class="btn">order now</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/images.jpg" alt="">
+            <div class="content">
+                <h3>Available</h3>
+                <p>ROTTWELIER</p>
+                <a href="#" class="btn">order now</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/images%20(7).jpg" alt="">
+            <div class="content">
+                <h3>Available</h3>
+                <p>HAVANESE</p>
+                <a href="#" class="btn">order now</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/images%20(8).jpg" alt="">
+            <div class="content">
+                <h3>Available</h3>
+                <p>ALASKAN MALAMUTE</p>
+                <a href="#" class="btn">order now</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/images%20(11).jpg" alt="">
+            <div class="content">
+                <h3>Available</h3>
+                <p>STANDARD POODLE</p>
+                <a href="#" class="btn">order now</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/images%20(14).jpg" alt="">
+            <div class="content">
+                <h3>Available</h3>
+                <p>PYRENEAN SHEPHERD</p>
+                <a href="#" class="btn">order now</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/images%20(13).jpg" alt="">
+            <div class="content">
+                <h3>Available</h3>
+                <p>GREAT DANE.</p>
+                <a href="#" class="btn">order now</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/images%20(18).jpg" alt="">
+            <div class="content">
+                <h3>Available</h3>
+                <p>ENGLISH MASTIFF</p>
+                <a href="#" class="btn">order now</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/images%20(15).jpg" alt="">
+            <div class="content">
+                <h3>Available</h3>
+                <p>GERMAN SHEPHERD</p>
+                <a href="#" class="btn">order now</a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="speciality" id="speciality">
+
+    <h1 class="heading">  <span></span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img class="image" src="images/download%20(3).jpg" alt="">
+            <div class="content">
+                <img src="images/download%20(3).jpg" alt="">
+                <h3>Maltese</h3>
+                <p</p>
+            </div>
+        </div>
+        <div class="box">
+            <img class="image" src="images/images%20(13).jpg" alt="">
+            <div class="content">
+                <img src="images/images%20(13).jpg" alt="">
+                <h3>GREAT DANE</h3>
+                <p></p>
+            </div>
+        </div>
+        <div class="box">
+            <img class="image" src="images/images%20(4).jpg" alt="">
+            <div class="content">
+                <img src="images/images%20(4).jpg" alt="">
+                <h3>pekingese</h3>
+                <p></p>
+            </div>
+        </div>
+        <div class="box">
+            <img class="image" src="images/images%20(5).jpg" alt="">
+            <div class="content">
+                <img src="images/images%20(5).jpg" alt="">
+                <h3>Chihuahua</h3>
+                <p></p>
+            </div>
+        </div>
+        <div class="box">
+            <img class="image" src="images/images%20(10).jpg" alt="">
+            <div class="content">
+                <img src="images/images%20(10).jpg" alt="">
+                <h3>Brussel grriffon</h3>
+                <p></p>
+            </div>
+        </div>
+        <div class="box">
+            <img class="image" src="images/images%20(1).jpg" alt="">
+            <div class="content">
+                <img src="images/images%20(1).jpg" alt="">
+                <h3>italian Greyhound</h3>
+                <p></p>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- speciality section ends -->
+
+<!-- popular section starts  -->
+
+
+<!-- steps section ends -->
+
+<!-- gallery section starts  -->
+
+
+<!-- gallery section ends -->
+
+<!-- review section starts  -->
+
+<section class="review" id="review">
+
+    <h1 class="heading"> our customers <span>reviews</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/pic1.png" alt="">
+            <h3>john deo</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <p>i just cant help but to love my beautiful ad healthy puppies from petsluv. best regards guys</p>
+        </div>
+        <div class="box">
+            <img src="images/pic2.png" alt="">
+            <h3>john deo</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <p>amazing and healthy Dogs.Big ups petsluv</p>
+        </div>
+        <div class="box">
+            <img src="images/pic3.png" alt="">
+            <h3>john deo</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <p>planning on getting a wife for my baby. expect my order real soon. love petloves</p>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- review section ends -->
+
+<!-- order section starts  -->
+
+<section class="order" id="order">
+
+    <h1 class="heading"> <span>order</span> now </h1>
+
+    <div class="row">
+        
+        <div class="image">
+            <img src="images/images%20(12).jpg" alt="">
+        </div>
+
+        <form action="">
+
+            <div class="inputBox">
+                <input type="text" placeholder="name">
+                <input type="email" placeholder="email">
+            </div>
+
+           
+
+            <textarea placeholder="Talk to us" name="" id="" cols="30" rows="10"></textarea>
+
+            <input type="submit" value="order now" class="btn">
+
+        </form>
+
+    </div>
+
+</section>
+
+<!-- order section ends -->
+
+<!-- footer section  -->
+
+<section class="footer">
+
+    <div class="share">
+        <a href="#" class="btn">facebook</a>
+        <a href="#" class="btn">twitter</a>
+        <a href="#" class="btn">instagram</a>
+        <a href="#" class="btn">pinterest</a>
+        <a href="#" class="btn">linkedin</a>
+    </div>
+
+<!--    <h1 class="credit"> created by <span> PhIl Chris </span> | all rights reserved! </h1>-->
+
+</section>
+
+<!-- scroll top button  -->
+<a href="#home" class="fas fa-angle-up" id="scroll-top"></a>
+
+<!-- loader  -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="script.js"></script>
+
+
+</body>
+</html>
